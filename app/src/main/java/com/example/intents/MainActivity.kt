682b0792008ab525ed961c760a5c2f6969fd0f42
Rafael Rocha Casaque <rrcasaque@hotmail.com>
2023-09-13 11:29:45 -0300

    feat: implementando a troca entre activities

--- app/src/main/java/com/example/intents/MainActivity.kt
+++ app/src/main/java/com/example/intents/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.intents
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.intents.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var amb: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        amb = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(amb.root)
+        supportActionBar?.subtitle = "MainActivity"
+
+        amb.entrarParametroBt.setOnClickListener {
+            val parametroIntent = Intent(this, ParametroActivity::class.java)
+            startActivity(parametroIntent)
+        }
     }
+
+
 }
---
+++ app/src/main/java/com/example/intents/ParametroActivity.kt
@@ -0,0 +1,18 @@
+package com.example.intents
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.intents.databinding.ActivityParametroBinding
+
+class ParametroActivity : AppCompatActivity() {
+    private val apb by lazy {
+        ActivityParametroBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(apb.root)
+        supportActionBar?.subtitle = "ParametroActivity"
+
+    }
+}
